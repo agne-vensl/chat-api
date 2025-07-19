2724442cf1655e2a781077d9550abf48140ede21
Agnė Venslavičiūtė <73463827+agne-vensl@users.noreply.github.com>
2025-07-19 16:27:09 +0300

    initial commit

---
+++ src/test/java/com/project/chatapi/ChatapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
